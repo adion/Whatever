1515b70ad723bff70f1c5755fd64c927c93930dc
adion <andredion@gmail.com>
2017-02-27 19:09:17 -0400

    A real model with animation!

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -10,23 +10,44 @@ public class PlayerController : MonoBehaviour {
 	public float fireRate;
 	
 	
+	private Animator animator;
 	private CharacterController cc;
 	private float currentJump;
 
+	private Vector3 lastLookDirection;
 	private float nextFire;
 
 	void Start () {
+		animator = GetComponent<Animator>();
 		cc = GetComponent<CharacterController>();
 	}
 	
 	void Update () {
 		Vector3 moveDirection = new Vector3(Input.GetAxisRaw("MoveHorizontal"), 0.0f, Input.GetAxisRaw("MoveVertical")).normalized;
 		Vector3 lookDirection = new Vector3(Input.GetAxisRaw("LookHorizontal"), 0.0f, Input.GetAxisRaw("LookVertical")).normalized;
+		
 		bool firing = lookDirection != Vector3.zero;
 
-		cc.SimpleMove(moveDirection * speed);
-		transform.rotation = Quaternion.LookRotation(lookDirection);
+		if (firing) {
+			lastLookDirection = lookDirection;
+		}
 
+		// move and look
+		cc.SimpleMove(moveDirection * speed);
+		transform.rotation = Quaternion.LookRotation(lastLookDirection);
+		
+		// animate
+		if (moveDirection == Vector3.zero) {
+			animator.Play("Idle");
+		} else if (moveDirection == -Vector3.forward && moveDirection != lookDirection) {
+			animator.SetBool("Walk Forward", false);
+			animator.SetBool("Walk Backward", true);
+		} else {
+			animator.SetBool("Walk Backward", false);
+			animator.SetBool("Walk Forward", true);
+		}
+		
+		// shoot?
 		if (firing && Time.time > nextFire) {
 			nextFire = Time.time + fireRate;
 			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
